acb843657c39c4ac96b4f71db3df5f463d16dcd7
Helene Boee <helebo@stud.ntnu.no>
2020-09-19 14:52:13 +0200

    Added serializer.

--- backend/recipe/admin.py
+++ backend/recipe/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'time', 'instruction')
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'food_type')
--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .serializers import RecipeSerializer
+from .models import Recipe
+
+class RecipeView(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all().order_by('title')
+    serializer_class = RecipeSerializer
+
+
+
