984bfdbe5b8277c04afc3837e9d5298cf9edf8a1
Helene Boee <helebo@stud.ntnu.no>
2020-09-19 13:20:14 +0200

    Created models.

--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    food_type = models.CharField(max_length=40)
+    
+
 class Recipe(models.Model):
     title = models.CharField(max_length=30)
     time = models.IntegerField()
     instruction = models.CharField(max_length=300)
     ingredients = models.ManyToManyField(Ingredient)
-
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=30)
-    food_type = models.CharField(max_length=40)
